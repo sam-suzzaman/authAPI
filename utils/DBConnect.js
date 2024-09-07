02f3c207a4c2a0dc4d68927db49de5b09a81776c
sam-suzzaman <dksamsuzzaman@gmail.com>
2024-09-07 19:30:22 +0600

    refactor

--- utils/DBConnect.js
+++ utils/DBConnect.js
@@ -3,12 +3,11 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 async function DBConnectionHandler() {
-    // {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //     }
     try {
-        await mongoose.connect(process.env.DB_STRING);
+        await mongoose.connect(process.env.DB_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("DB connected successfully");
     } catch (err) {
         console.log(`There is an error id DB: ${err.message}`);
