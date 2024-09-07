25bfcc33cad3a34259248961acb85b42a236c18c
sam-suzzaman <dksamsuzzaman@gmail.com>
2024-09-07 20:13:19 +0600

    refactor

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,4 @@
-const UserModel = require("../model/UserModel");
+const UserModel = require("../model/userModel");
 const bcrypt = require("bcrypt");
 const JWTGenerator = require("../utils/JWTGenerator");
 // const JWTGenerator = require("../utilities/JWT_Generator");
